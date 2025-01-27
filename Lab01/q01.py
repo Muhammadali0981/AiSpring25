5cb40d83cf2059baca1b434f8d10110efa4f872c
Muhammad Ali <muhammadali030605@gmail.com>
2025-01-27 10:22:56 +0500

    Update q01.py

--- Lab01/q01.py
+++ Lab01/q01.py
@@ -8,21 +8,28 @@ class system:
             self.comps[chr(ord('a') + i)] = random.choice([0, 1])
 
 class agent:
-    def __init__(self, i_comp):
-        self.agent_comp = i_comp
+    def __init__(self):
         self.vurns = []
-    
-    def scan(self):
-        for key in self.agent_comp:
-            if self.agent_comp[key] == 1:
-                self.vurns.append(key)
-        
-    def patches(self):
-        for i in range(len(self.vurns)):
-            self.agent_comp[self.vurns[i]] == 1
-            print(f'vurniblity at {self.vurns[i]} patched')
         
 
+    def scan(self, i_comp):
+        for key in i_comp:
+            if i_comp[key] == 1:
+                self.vurns.append(key)
+        
+    def patches(self, i_comp):
+        for i in range(len(self.vurns)):
+            i_comp[self.vurns[i]] == 1
+            print(f'vurnebility at {self.vurns[i]} patched')
+            patched = i_comp
+        return patched
+    
+def run_agent(agent, system):
+    percept = system.comps
+    agent.scan(percept)
+    system.comps = agent.patches(percept)
+
+
 mysystem = system()
-myagent = agent(mysystem.comps)
-myagent.patches()
+myagent = agent()
+run_agent(myagent, mysystem)
